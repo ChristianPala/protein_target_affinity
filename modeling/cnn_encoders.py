c08f9e1d20d211aaed0047e33701d97aad61ed6c
Christian Pala <pala.christian@gmail.com>
2023-05-21 20:31:37 +0200

    Added Notebook KiBA Davis Andrea.

--- modeling/cnn_encoders.py
+++ modeling/cnn_encoders.py
@@ -4,12 +4,12 @@ from torch import nn
 from dti_model import DrugTargetNET
 
 
-class ConvBlock(nn.Module):
+class ConvBlockSMILES(nn.Module):
     """
-    Class to create a convolutional block for the CNN encoders for SMILES and protein sequences.
+    Class to create a 1D convolutional block to encode SMILES sequences from Morgan fingerprints of length 1024.
     """
     def __init__(self, in_channels: int):
-        super(ConvBlock, self).__init__()
+        super(ConvBlockSMILES, self).__init__()
         self.conv1 = nn.Conv1d(in_channels, 64, kernel_size=3, stride=1, padding=1)
         self.relu = nn.ReLU()
         self.conv2 = nn.Conv1d(64, 1024, kernel_size=3, stride=1, padding=1)
@@ -24,6 +24,28 @@ class ConvBlock(nn.Module):
         x = self.adaptive_maxpool(x)
         return x
 
+class ConvBlockProtein(nn.Module):
+    """
+    Class to create a 2D convolutional block to encode protein sequences from the protein encoding of length 1200.
+    """
+    def __init__(self, in_channels: int):
+        super(ConvBlockProtein, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, 64, kernel_size=(3,3), stride=1, padding=1)  # Change to Conv2d
+        self.relu = nn.ReLU()
+        self.conv2 = nn.Conv2d(64, 1024, kernel_size=(3,3), stride=1, padding=1)  # Change to Conv2d
+        self.adaptive_maxpool = nn.AdaptiveMaxPool2d((1,1))  # Change to AdaptiveMaxPool2d
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.adaptive_maxpool(x)
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.adaptive_maxpool(x)
+        x = x.view(x.size(0), -1)  # Flatten the output for FCN
+        return x
+
+
 
 class CNNEncoderModel(nn.Module):
     """
@@ -34,8 +56,9 @@ class CNNEncoderModel(nn.Module):
         super(CNNEncoderModel, self).__init__()
         self.smiles_features = smiles_encoding
         self.protein_features = protein_encoding
-        self.cnn_smiles = ConvBlock(smiles_encoding)
-        self.cnn_protein = ConvBlock(protein_encoding)
+        self.cnn_smiles = ConvBlockSMILES(smiles_encoding)
+        self.cnn_protein = ConvBlockProtein(protein_encoding)
+        # connect the two encodings to the DrugTargetNET model with the Öztürk et al. architecture
         self.fcn = DrugTargetNET(self.smiles_features, self.protein_features, dropout_p)
 
     def forward(self, x_smiles, x_protein):
